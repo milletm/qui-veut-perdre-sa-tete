bc547851ce3367c61ae00b8619c74507f6dceaec
millet <millet.martin31@gmail.com>
2020-01-21 21:54:46 +0100

    Updates

---
+++ precache-manifest.6a4e385ac9de95b7625a317a7bf39f5a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2f2f3e8f38a1c74200d73e5af201f55",
+    "url": "/qui-veut-perdre-sa-tete/index.html"
+  },
+  {
+    "revision": "d2f035a228cb44108996",
+    "url": "/qui-veut-perdre-sa-tete/static/css/main.bf46a3c4.chunk.css"
+  },
+  {
+    "revision": "52784b107c64d637ee63",
+    "url": "/qui-veut-perdre-sa-tete/static/js/2.87cae2d8.chunk.js"
+  },
+  {
+    "revision": "730c58bfa296c939810827407eb2fe6c",
+    "url": "/qui-veut-perdre-sa-tete/static/js/2.87cae2d8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d2f035a228cb44108996",
+    "url": "/qui-veut-perdre-sa-tete/static/js/main.96828a7e.chunk.js"
+  },
+  {
+    "revision": "a63cb08fab8db103e49d",
+    "url": "/qui-veut-perdre-sa-tete/static/js/runtime-main.b2064d3e.js"
+  },
+  {
+    "revision": "7ab5d8a602ffb4cd8245c1039a0de276",
+    "url": "/qui-veut-perdre-sa-tete/static/media/BalonkuRegular-la1w.7ab5d8a6.otf"
+  },
+  {
+    "revision": "ed6814fded88569f0985ba66b3786ae2",
+    "url": "/qui-veut-perdre-sa-tete/static/media/buzzerActive.ed6814fd.svg"
+  },
+  {
+    "revision": "cc0df625cc5dd70e985257f7482d73bc",
+    "url": "/qui-veut-perdre-sa-tete/static/media/buzzerInactive.cc0df625.svg"
+  }
+]);
